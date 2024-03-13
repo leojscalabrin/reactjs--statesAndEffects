3aa7dbfaabad5c5e99d3a44272320cbf8332c1bb
Leonardo Scalabrin <leojscalabrin@gmail.com>
2024-03-13 11:31:18 -0300

    smart counter

---
+++ aula-hooks/src/Components/App/App.jsx
@@ -0,0 +1,13 @@
+import SmartCounter from "../Smartcounter/Smartcounter";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Bonito Contador</h1>
+      <SmartCounter />
+    </>
+  );
+}
+
+export default App;
