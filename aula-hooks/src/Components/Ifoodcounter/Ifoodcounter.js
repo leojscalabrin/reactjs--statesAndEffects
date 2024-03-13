ddd340c43e6e4fcfdd2800d81d648de7904b620f
Leonardo Scalabrin <leojscalabrin@gmail.com>
2024-03-13 11:56:39 -0300

    finish

--- aula-hooks/src/Components/Ifoodcounter/Ifoodcounter.js
+++ aula-hooks/src/Components/Ifoodcounter/Ifoodcounter.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../Ifoodcounter/Ifoodcounter.css";
 
 export default function Ifoodcounter() {
   const [value, setValue] = useState(1);
   const [buttonStyle, setButtonStyle] = useState("counter-button-minus-active");
+
+  // useEffect(()=> {
+  //   document.getElementById("moeda").innerHTML = 2.00 * value
+  // },[value])
+
+  useEffect(()=> {
+    alert('Você não pode mais diminuir itens.')
+  },[buttonStyle])
 
   function down() {
     if (value <= 1) {
@@ -29,6 +37,7 @@ export default function Ifoodcounter() {
       <button className="counter-button-plus-active" onClick={up}>
         +
       </button>
+      <button id="moeda">12,00</button>
     </div>
   );
 }
